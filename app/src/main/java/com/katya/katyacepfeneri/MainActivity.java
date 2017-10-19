6ee4f4487029b8c34c72af7374dc97ab87cda9bf
ocolak <orcuncolak@yahoocom>
2017-10-19 20:46:33 +0300

    test

---
+++ app/src/main/java/com/katya/katyacepfeneri/MainActivity.java
@@ -0,0 +1,13 @@
+package com.katya.katyacepfeneri;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
