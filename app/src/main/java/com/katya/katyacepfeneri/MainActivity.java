1027b10340de024d2e77b27cb4b17b0a79f80ade
ocolak <orcuncolak@yahoocom>
2017-10-22 08:05:01 +0300

    Working version

--- app/src/main/java/com/katya/katyacepfeneri/MainActivity.java
+++ app/src/main/java/com/katya/katyacepfeneri/MainActivity.java
@@ -1,14 +1,81 @@
 package com.katya.katyacepfeneri;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 //Main window
 public class MainActivity extends Activity {
+
+    boolean hasFlash;
+    Button feneriAc;
+    Button feneriKapa;
+    Camera camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //This is called only once
+        camera = Camera.open();
+
+        feneriAc =  (Button)findViewById(R.id.buttonAc);
+        feneriKapa = (Button)findViewById(R.id.buttonKapa);
+
+        hasFlash = getApplicationContext().getPackageManager().
+                hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        feneriAc.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick (View v) {
+
+                if (isLightOn () == false) {
+                   setLightOn();
+                }
+            }
+        });
+
+        feneriKapa.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick (View v) {
+
+                if (isLightOn ()) {
+                    setLightOff();
+                }
+            }
+        });
+    }
+
+    protected void onDestroy () {
+        //This is called when back button is pressed
+        camera.release();
+    }
+
+    private void setLightOff () {
+        Parameters params = camera.getParameters();
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        camera.setParameters(params);
+        camera.stopPreview();
+
+    }
+
+    private void setLightOn () {
+        Parameters params = camera.getParameters();
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(params);
+        camera.startPreview();
+
+    }
+
+    private boolean isLightOn () {
+        return Parameters.FLASH_MODE_TORCH.equals(camera.getParameters().getFlashMode());
+
     }
 }
